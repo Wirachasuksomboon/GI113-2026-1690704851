a2b3d5ec9afcf8943592825a7dd0e33ed24a381b
Wirachasuksomboon <Wiracha.suks@bumail.net>
2026-08-25 17:11:37 +0700

    add lab 02

---
+++ Lab02/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            //lab02 A
+            Console.WriteLine("==== BOOS STATUS INITAL ====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"level: {level}");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            Console.WriteLine($"Attack power: {attackPower}");
+            Console.WriteLine($""
+
+
+
+
+        }
+    }
+}
